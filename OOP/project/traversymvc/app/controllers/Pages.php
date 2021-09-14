05837b820b4793ef1b5b7038c31d31e023ccfcfd
cesar celis <celis212@gmail.com>
2021-09-13 21:48:37 -0500

    09/13/2021

--- OOP/project/traversymvc/app/controllers/Pages.php
+++ OOP/project/traversymvc/app/controllers/Pages.php
@@ -1,7 +1,30 @@
 <?php
-    class Pages {
+    class Pages extends Controller{
         public function __constructor(){
-            echo 'Pages loaded';
+            //echo 'Pages loaded';
+        }    
+        
+        public function index(){
+            //example to prove the view module
+           // $this->view('Hello');
+           $data = [
+            'title' => 'Welcome'
+           ];
+           $this->view('pages/index', $data);
+        }
+
+        public function about(){//about($id){
+            //echo $id;
+            //$this->view('pages/about');
+            $data = [
+                'title' => 'About Us'
+            ];
+            $this->view('pages/about', $data);
         }
     }
+
+
+
+
+
     
